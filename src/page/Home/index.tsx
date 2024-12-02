f35a99684caf165ab76622fbe201d97e4b23cb73
Larissa <larissa_gomes19@hotmail.com>
2024-12-02 10:57:03 -0300

    fix: fix bug

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,14 +1,13 @@
 import Heading from '../../components/Heading';
 import Filter from '../../components/Filter';
 import PokemonList from '../../components/PokemonList';
-import ServiceProvider from '../../provider/ServiceProvider';
 
 export default function Home() {
   return (
-    <ServiceProvider>
+    <>
       <Heading>Encontre seu Pokémon</Heading>
       <Filter />
       <PokemonList />
-    </ServiceProvider>
+    </>
   );
 }
